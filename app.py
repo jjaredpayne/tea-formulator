fb4aea88880e5d00aaa17ffef139211361ba939b
jjaredpayne <payneje@oregonstate.edu>
2021-10-29 21:31:27 -0700

    Added Flavor profiles. Need to fix JSON

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import json
 import urllib.parse
 
@@ -10,51 +10,120 @@ app = Flask(__name__)
 #     self.latinbinomial = latinbinomial
 #     self.plantpart = plantpart
 
-melissaofficinalis = {"commonname": "Lemon Balm", "latinbinomial": "Melissa officinalis", "plantpart": "Leaf"}
-menthaspicata = {"commonname": "Spear Mint", "latinbinomial": "Mentha spicata", "plantpart": "Leaf"}
-cinnamomumcassia = {"commonname": "Cassoa", "latinbinomial": "Cinnamomum cassia", "plantpart": "Bark"}
+melissaofficinalis = {
+        "flavors": [
+        {
+            "B": "1",
+            "Sa": "0",
+            "So": "0",
+            "Sw": "0",
+            "U": "0",
+            "C": "0",
+            "E": "0",
+            "Fl": "0",
+            "Fr": "0",
+            "He": "0",
+            "Ho": "0",
+            "N": "0",
+            "Pi": "0",
+            "Pu": "0",
+            "Sp": "0",
+            "Su": "0",
+            "W": "0",
+        }
+    ],
+    "commonname": "Lemon Balm",
+    "latinbinomial": "Melissa officinalis",
+    "plantpart": "Leaf",
+}
+menthaspicata = {
+    "commonname": "Spear Mint",
+    "latinbinomial": "Mentha spicata",
+    "plantpart": "Leaf"
+}
+cinnamomumcassia = {"commonname": "Cassia",
+    "latinbinomial": "Cinnamomum cassia",
+    "plantpart": "Bark"
+}
+phytolaccaamericana = {
+    "commonname": "Poke",
+    "latinbinomial": "Phytolacca americana",
+    "plantpart": "Fruit"
+}
+hypericumperforatum = {
+    "commonname": "St. John's Wort",
+    "latinbinomial": "Hypericum perforatum",
+    "plantpart": "Leaf"
+}
+passifloraincarnata = {
+    "commonname": "Passionflower",
+    "latinbinomial": "Passiflora incarnata",
+    "plantpart": "Leaf and Stem"
+}
+centellaasiatica = {
+    "commonname": "Gotu Kola",
+    "latinbinomial": "Centella asiatica",
+    "plantpart": "Leaf and Stem"
+}
+arctostaphylosuvaursi = {
+    "commonname": "Kinnickinick",
+    "latinbinomial": "Arctostaphylos uvaursi",
+    "plantpart": "Leaf"
+}
+curcumalonga = {
+    "commonname": "Turmeric",
+    "latinbinomial": "Curcuma longa",
+    "plantpart": "Rhizome"
+}
 
 HerbList = []
 TeaList = []
 
+
 @app.route("/", methods=['GET', 'POST'])
 def teamain():
 
-    # melissaofficinalis = Herb("Lemon Balm", "Melissa officinalis", "Leaf")
-    # menthaspicata = Herb( "Spear Mint", "Mentha spicata", "Leaf" )
-    # cinnamomumcassia = Herb("Cassia", "Cinnamomum cassia", "Bark")
-    # phytolaccaamericana = Herb("Poke", "Phytolacca americana", "Fruit")
-    # hypericumperforatum = Herb("St. John's Wort", "Hypericum perforatum", "Leaf and Stems")
-    # passifloraincarnata = Herb("Passionflower", "Passiflora incarnata", "Leaf and Stems")
-    # centellaasiatica = Herb("Gotu Kola", "Centella Asiatica", "Leaf")
-    # arctostaphylosuvaursi = Herb("Kinnickinick", "Arctostaphylos uva-ursi", "Leaf")
-    # curcumalonga = Herb("Tumeric", "Curcuma long", "Rhizome")
-
-    print(melissaofficinalis)
-    print(type(melissaofficinalis))
     HerbList.append(melissaofficinalis)
     HerbList.append(menthaspicata)
     HerbList.append(cinnamomumcassia)
-    # HerbList.append(phytolaccaamericana)
-    # HerbList.append(hypericumperforatum)
-    # HerbList.append(passifloraincarnata)
-    # HerbList.append(centellaasiatica)
-    # HerbList.append(arctostaphylosuvaursi)
-    # HerbList.append(curcumalonga)
+    HerbList.append(phytolaccaamericana)
+    HerbList.append(hypericumperforatum)
+    HerbList.append(passifloraincarnata)
+    HerbList.append(centellaasiatica)
+    HerbList.append(arctostaphylosuvaursi)
+    HerbList.append(curcumalonga)
 
     print("HerbList " + str(HerbList))
 
-    return render_template("index.html", HerbList = HerbList, TeaList = TeaList)
+    return render_template("index.html", HerbList=HerbList, TeaList=TeaList)
+
 
 @app.route("/AddToTea", methods=['GET', 'POST'])
 def addHerb():
     if request.method == "GET":
         addedHerb = request.args.get('fullHerb', '')
-        addedHerb = addedHerb.replace("'",'"')
+        addedHerb = addedHerb.replace("'", '"')
         herbJSON = json.loads(addedHerb)
         print(herbJSON)
-        print(type(herbJSON))    
+        print(type(herbJSON))
         TeaList.append(herbJSON)
         print("teaList" + str(TeaList))
 
-    return str(TeaList)
+    return jsonify(TeaList)
+
+
+@app.route("/RemoveFromTea", methods=['GET', 'POST'])
+def removeHerb():
+    if request.method == "GET":
+        latinbinomial = request.args.get('latinbinomial', '')
+        print(latinbinomial)
+        print(type(latinbinomial))
+        for herb in TeaList:
+            print(herb)
+            print(type(str(herb)))
+            if latinbinomial in str(herb):
+                TeaList.remove(herb)
+                print("Herb Removed " + str(herb))
+        print("After Removal" + str(TeaList))
+
+    return jsonify(TeaList)
--- static/main.js
+++ static/main.js
@@ -41,6 +41,72 @@ function UpdateHerbInfo(){
         document.getElementById('herbName').innerText = selectedHerbName;
         document.getElementById('herbText').innerText = selectedHerbText;
 
+        var B = "<div>1</div>"
+        var Sa = "<div>2</div>"
+        var Sw = "<div>3</div>"
+        var So = "<div>4</div>"
+        var U = "<div>5</div>"
+        var C= "<div>6</div>"
+        var E = "<div>7</div>"
+        var Fl = "<div>8</div>"
+        var Fr = "<div>9</div>"
+        var He = "<div></div>"
+        var Ho = "<div></div>"
+        var N = "<div></div>"
+        var Pi = "<div></div>"
+        var Pu = "<div></div>"
+        var Sp = "<div></div>"
+        var Su = "<div></div>"
+        var W = "<div></div>"
+
+        fullHerb = fullHerb.replaceAll("'", '"');
+        console.log("fullherb" + fullHerb)
+        console.log("fullherb" + fullHerb["flavors"])
+        console.log("fullherb" + fullHerb[0])
+        console.log("fullherb" + fullHerb[1])
+
+        if (fullHerb[0].B == "1")
+            B="<div>Bitter</div>"
+        if (fullHerb[0].Sa == 1)
+            Sa= "<div>Salty</div>\n"
+        if (fullHerb[0].Sw == 1)
+            Sw= "<div>Sweet</div>"
+        if (fullHerb[0].So == 1)
+            So= "<div>Sour</div>"
+        if (fullHerb[0].U == 1)
+            U= "<div>Umami</div>"
+        if (fullHerb[0].C == 1)
+            Co= "<div>Cooling</div>"
+        if (fullHerb[0].E == 1)
+            Ea= "<div>Earthy</div>"
+        if (fullHerb[0].Fl == 1)
+            Fl= "<div>Floral</div>"
+        if (fullHerb[0].Fr == 1)
+            Fr= "<div>Fruit</div>";
+        if (fullHerb[0].He == 1)
+            He="<div>Herbaceous</div>"
+        if (fullHerb[0].Ho == 1)
+            Ho= "<div>Hot</div>"
+        if (fullHerb[0].N == 1)
+            N= "<div>Nutty</div>"
+        if (fullHerb[0].Pi == 1)
+            Pi= "<div>Piney</div>"
+        if (fullHerb[0].Pu == 1)
+            Pu= "<div>Pungent</div>"
+        if (fullHerb[0].Sa == 1)
+            Sp= "<div>Spicy</div>"        
+        if (fullHerb[0].Sp == 1)
+            Su= "<div>Sulfury</div>"
+        if (fullHerb[0].W == 1)
+            W= "<div>Woody</div>"
+
+        c1 = B + Sa + Sw + So + U + C + E + Fl + Fr
+        c2 = He + Ho + N + Pi + Pu + Sp + Su + W
+        console.log(B)
+
+        document.getElementById('herbFlavorC1').innerHTML = c1;
+        document.getElementById('herbFlavorC2').innerHTML = c2;
+
         confirmedHerb = fullHerb
 
     });
@@ -50,8 +116,8 @@ function AddToTea() {
         return new Promise(function (resolve, reject) {
             // document.getElementById('addToTeaButton').addEventListener('click', function (event) {
                 console.log("fullHerb")
-                var parsedFullHerb = JSON.parse(fullHerb.replaceAll("'", '"'));    
-                var confirmedlatinebinomial = parsedFullHerb.latinbinomial;    
+                //var parsedFullHerb = JSON.parse(fullHerb.replaceAll("'", '"'));    
+                //var confirmedlatinebinomial = parsedFullHerb.latinbinomial;    
                 var req = new XMLHttpRequest();
 
                 reqURL = indexUrl + 'AddToTea' + '?fullHerb=' + fullHerb;
@@ -97,6 +163,7 @@ function AddToTea() {
                                     +"</li>"
                             }
                             document.getElementById('teaListEntries').innerHTML = teaListEntries
+                            // documentFragment.getElementById('teaFlavor').innerHTML = teaListEntries
                         } 
                         else {
                             console.log('error: reponse empty');
@@ -122,7 +189,7 @@ function RemoveFromTea() {
             var req = new XMLHttpRequest();
 
             reqURL = indexUrl + 'RemoveFromTea' + '?latinbinomial=' + tealatinbinomial;
-            console.log('reqURL:', reqURL);
+            //console.log('reqURL:', reqURL);
             req.open('GET', reqURL, true);
             req.addEventListener("load", function () {
                 if (req.status >= 200 && req.status < 400) {
@@ -130,15 +197,15 @@ function RemoveFromTea() {
 
                         var TeaList = req.responseText;
 
-                        console.log("TeaList: " + TeaList)
+                        //console.log("TeaList: " + TeaList)
                         TeaList = JSON.parse(TeaList)
-                        console.log(typeof TeaList)
-                        console.log(TeaList)
+                        //console.log(typeof TeaList)
+                        //console.log(TeaList)
 
                         // console.log("JSON.parse: " + JSON.parse(req.responseText));
                         // let response = JSON.parse(req.responseText);
 
-                        console.log(TeaList.length);
+                        //console.log(TeaList.length);
 
 
                         var teaListEntries =  ''
@@ -149,9 +216,9 @@ function RemoveFromTea() {
                             // console.log(typeof TeaList)
                             // console.log("CURRENT TeaList: " + TeaList.commonname)
                             // console.log("CURRENT Herb: " )
-                            // console.log(TeaList[i] )
+                            console.log(TeaList[i] )
                             // currentHerb = TeaList[i]
-                            //currentHerb= JSON.parse(TeaList[i])
+                            // currentHerb= JSON.parse(TeaList[i])
                             currentHerb = TeaList[i]
 
                             teaListEntries += "<a href=\"#!\" class=\"collection-item avatar\" onclick=\"SelectTeaHerb('"+ TeaList[i].commonname + "', '"+TeaList[i].latinbinomial + "')\">"
